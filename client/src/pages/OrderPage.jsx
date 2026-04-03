48c49411fcac390316c85f7541d2253b1a0f96f8
Eanthonycarranza <anthonycarranza123@gmail.com>
2026-04-03 13:40:22 -0500

    Fix syntax error in OrderPage.jsx

--- client/src/pages/OrderPage.jsx
+++ client/src/pages/OrderPage.jsx
@@ -61,7 +61,7 @@ export default function OrderPage() {
         </div>
 
         <div className="order-options">
-...
+          <div className="order-option">
             <svg width="60" height="60" viewBox="0 0 24 24" fill="none" stroke="#6b4c7a" strokeWidth="1.5">
               <path d="M3 9l9-7 9 7v11a2 2 0 01-2 2H5a2 2 0 01-2-2z"/><polyline points="9 22 9 12 15 12 15 22"/>
             </svg>
