1d03ade8ea032514997d52f8b3da0dd73ec043ad
Eanthonycarranza <anthonycarranza123@gmail.com>
2026-04-03 00:05:30 -0500

    chore: add Heroku deployment configurations

--- server/index.js
+++ server/index.js
@@ -23,5 +23,15 @@ app.use('/api/reviews', require('./routes/reviews'));
 app.use('/api/settings', require('./routes/settings'));
 app.use('/api/admin', require('./routes/admin'));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static(path.join(__dirname, '../client/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
